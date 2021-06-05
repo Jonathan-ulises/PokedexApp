881c3ab077270aa7189507a2fc8fb4e5536104dc
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-06-04 19:13:38 -0500

    Creacion del contenedor de fragments/Intercambio entre fragments

--- app/src/main/java/com/macrobios/pokedex/MainActivity.java
+++ app/src/main/java/com/macrobios/pokedex/MainActivity.java
@@ -2,12 +2,23 @@ package com.macrobios.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.macrobios.pokedex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements PokemonListFragment.OnPokemonSelectedListener{
+
+    private final int OPTION_DETAIL = 0;
+    private final int OPTION_STATS = 1;
+
+    private Pokemon selectedPokemon;
+    private int selectedOption;
+
+    private Fragment currentFragment;
 
     private ActivityMainBinding binding;
     @Override
@@ -21,10 +32,12 @@ public class MainActivity extends AppCompatActivity implements PokemonListFragme
 
         binding.btnImage.setOnClickListener(v -> {
             changeColorButton(v.getId());
+            selFragmentAndContent();
         });
 
         binding.btnStats.setOnClickListener(v -> {
             changeColorButton(v.getId());
+            selFragmentAndContent();
         });
     }
 
@@ -33,18 +46,37 @@ public class MainActivity extends AppCompatActivity implements PokemonListFragme
             case R.id.btnImage:
                 binding.btnImage.setBackgroundColor(ContextCompat.getColor(this, R.color.option_button_selected));
                 binding.btnStats.setBackgroundColor(ContextCompat.getColor(this, R.color.option_button_not_selected));
+                selectedOption = OPTION_DETAIL;
                 break;
             case R.id.btnStats:
                 binding.btnImage.setBackgroundColor(ContextCompat.getColor(this, R.color.option_button_not_selected));
                 binding.btnStats.setBackgroundColor(ContextCompat.getColor(this, R.color.option_button_selected));
+                selectedOption = OPTION_STATS;
                 break;
         }
     }
 
     @Override
     public void onPokemonItemList(Pokemon pokemon) {
-        DetailFragment fragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_fragment);
+        selectedPokemon = pokemon;
+        selFragmentAndContent();
+    }
 
-        
+    private void selFragmentAndContent(){
+        if(selectedPokemon != null){
+            if(selectedOption == OPTION_DETAIL) {
+                currentFragment = DetailFragment.newInstance(selectedPokemon.getImageUrl(), selectedPokemon.getSoundID());
+            } else if (selectedOption == OPTION_STATS) {
+                currentFragment = StatsFragment.newInstance(selectedPokemon.getStats());
+            }
+
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.detail_fragment_container, currentFragment);
+            fragmentTransaction.commit();
+        } else {
+            Toast.makeText(this, "You have not selected a pokemon", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
